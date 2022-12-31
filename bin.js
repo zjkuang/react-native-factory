41dfd1b4d771f607a1a8425697b4330c98427c32
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-31 09:31:59 -0600

    in progress

--- bin.js
+++ bin.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 import appRootDir from 'app-root-dir';
+import {make} from './src/maker/index.js';
 
 console.log('bin.js > Hello, world!');
 
 console.log(`appRootDir=${appRootDir.get()}`);
 console.log(`cwd=${process.cwd()}`);
+
+make();
--- src/maker/index.js
+++ src/maker/index.js
@@ -1,14 +1,27 @@
 import {program} from 'commander';
 import fs from 'fs';
 
-export const make = () => {
+const parseArgs = () => {
+  const result = {};
   program
   .name('@zjkuang/react-native-factory')
   .description('Create a React Native project in TypeScript with specific template.')
-  .usage('npx @zjkuang/react-native-factory init <app-name> [--template {default|<other>}]')
-  .option('--template <template>', 'app template')
-  .command('init <project>')
-  .action((project, template) => {
-    console.log(`project: ${project}, template: ${template}`);
-  })
+  .usage('npx @zjkuang/react-native-factory init <app-name> [--template {default|<other>}]');
+  program
+  .command('init <app>')
+  .option('-t, --template <template>', 'app template')
+  .action((app, options) => {
+    // $ npx . init MyApp --template default
+    // >>> app: MyApp, options: {"template":"default"}
+    // console.log(`>>> app: ${app}, options: ${JSON.stringify(options)}`); // app: MyApp, options: {"template":"default"}
+    result.appName = app;
+    result.template = options.template;
+  });
+  program.parse();
+  return result;
+};
+
+export const make = () => {
+  const args = parseArgs();
+  console.log(`>>> app: ${args.appName}, template: ${args.template}`);
 };
