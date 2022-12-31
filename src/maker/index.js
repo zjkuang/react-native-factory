7682f3259c919d2ef2a2b546916235bc2809f1a7
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-31 10:49:21 -0600

    in progress

--- src/maker/index.js
+++ src/maker/index.js
@@ -1,4 +1,5 @@
 import {program} from 'commander';
+import appRootDir from 'app-root-dir';
 import fs from 'fs';
 
 const parseArgs = () => {
@@ -21,7 +22,32 @@ const parseArgs = () => {
   return result;
 };
 
+const createApp = (args) => {
+  const appName = args.appName;
+  if (!appName || String(appName).trim() === '') {
+    console.log('Missing app name.');
+    return;
+  }
+  if (fs.existsSync(appName)) {
+    console.log(`${appName} already exists in current directory.`);
+    return;
+  }
+  const template = args.template || 'default';
+  const validTemplates = ['default'];
+  if (!validTemplates.includes(template)) {
+    console.log(`Unknown template: ${template}`);
+  }
+  fs.mkdirSync(appName);
+  const templatesDir = appRootDir.get() + '/node_modules/@zjkuang/react-native-factory/template-apps';
+  let templateDir;
+  if (template === 'default') {
+    templateDir = templatesDir + '/default';
+  }
+  console.log(`Creating app from template location ${templateDir}`);
+};
+
 export const make = () => {
   const args = parseArgs();
   console.log(`>>> app: ${args.appName}, template: ${args.template}`);
+  createApp(args);
 };
