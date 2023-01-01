c2f8c037452b6f85d063b855e557010a3fa8bc42
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-31 21:11:58 -0600

    ios app name

--- src/maker/index.js
+++ src/maker/index.js
@@ -1,7 +1,7 @@
 import {program} from 'commander';
 import appRootDir from 'app-root-dir';
 import {copy} from 'fs-extra';
-import fs from 'fs';
+import {existsSync, mkdirSync, readFileSync, writeFileSync} from 'fs';
 
 const parseArgs = () => {
   const result = {};
@@ -23,13 +23,33 @@ const parseArgs = () => {
   return result;
 };
 
-const createApp = (args) => {
+const updateAppName = async (appName) => {
+  const infoPlist = `${appName}/ios/app/info.plist`;
+  const launchScreenStoryboard = `${appName}/ios/app/LaunchScreen.storyboard`;
+  try {
+    const [infoPlistText, launchScreenStoryboardText] = await Promise.all([
+      readFileSync(infoPlist).toString(),
+      readFileSync(launchScreenStoryboard).toString(),
+    ]);
+    const [updatedInfoPlistText, updatedLaunchScreenStoryboardText] = [
+      infoPlistText.replace('<string>app</string>', `<string>${appName}</string>`),
+      launchScreenStoryboardText.replace('text="app"', `text="${appName}"`),
+    ];
+    writeFileSync(infoPlist, updatedInfoPlistText);
+    writeFileSync(launchScreenStoryboard, updatedLaunchScreenStoryboardText);
+  } catch (error) {
+    throw error;
+  }
+  return;
+};
+
+const createApp = async (args) => {
   const appName = args.appName;
   if (!appName || String(appName).trim() === '') {
     console.log('Missing app name.');
     return;
   }
-  if (fs.existsSync(appName)) {
+  if (existsSync(appName)) {
     console.log(`${appName} already exists in current directory.`);
     return;
   }
@@ -38,7 +58,7 @@ const createApp = (args) => {
   if (!validTemplates.includes(template)) {
     console.log(`Unknown template: ${template}`);
   }
-  fs.mkdirSync(appName);
+  mkdirSync(appName);
   const templatesDir = appRootDir.get() + '/node_modules/@zjkuang/react-native-factory/template-apps';
   let templateDir;
   if (template === 'default') {
@@ -48,20 +68,12 @@ const createApp = (args) => {
   console.log(`Creating app from template location ${templateDir}`);
 
   let finished = false;
-  copy(templateDir, appName)
-  .then(() => {
-    console.log('Done!');
-  })
-  .catch((error) => {
-    console.log(`Failed. ${JSON.stringify(error)}`);
-  })
-  .finally(() => {
-    finished = true;
-  });
   let count = 0;
-  let hourglass = setInterval(() => {
+  let hourglass = setInterval(async () => {
     if (finished) {
       clearInterval(hourglass);
+      await updateAppName(appName);
+      console.log('Done!');
       return;
     }
     count++;
@@ -73,6 +85,12 @@ const createApp = (args) => {
       process.stdout.write('.');
     }
   }, 500);
+  try {
+    await copy(templateDir, appName);
+  } catch (error) {
+    console.log(`Failed. ${JSON.stringify(error)}`);
+  }
+  finished = true;
 };
 
 export const make = () => {
